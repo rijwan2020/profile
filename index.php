f8fb88e8b1e4a3f401f35e65d8fe0be2bba304a4
Rijwan <rijwan@4visionmedia.com>
2021-11-02 16:16:51 +0700

    tes

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus pariatur soluta nulla suscipit! Quas assumenda laboriosam commodi nesciunt officiis alias consequuntur id, beatae necessitatibus quasi eveniet tempora mollitia molestiae reiciendis?
+</body>
+</html>
